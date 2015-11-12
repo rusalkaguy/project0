68752deabf6432bca46d62f78f468880385d6a35
bheater <bheater88@gmail.com>
2015-11-12 13:44:35 -0600

    Finalized working version of Write_to_GFF.py.

--- Write_to_GFF.py
+++ Write_to_GFF.py
@@ -1,9 +1,14 @@
-#Write Genbank File to GFF file format: http://biopython.org/wiki/GFF_Parsing
+# Defining Accession Number, File_name, and Database
+accession_number= 'U00096'
+file_name = accession_number + '.download.gbk'
+db = 'nucleotide' 
+
+# Write Genbank File to GFF file format: http://biopython.org/wiki/GFF_Parsing
 from BCBio import GFF
 from Bio import SeqIO
  
-in_file = "your_file.gb"
-out_file = "your_file.gff"
+in_file = file_name
+out_file = accession_number + '.gff'
 in_handle = open(in_file)
 out_handle = open(out_file, "w")
  
