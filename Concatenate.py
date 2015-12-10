f6da77890c53d761c963056ab1ae07225bf3181b
bheater <bheater88@gmail.com>
2015-12-10 16:44:13 -0600

    Added for loop, but it doesn't work because the program doesn't write the bed or gff file correctly.

--- Concatenate.py
+++ Concatenate.py
@@ -13,62 +13,64 @@ accession_number = argv[1]
 file_name = accession_number + '.gbk'
 db = 'nucleotide'
 
-# Grabbing genomes from Genbank
-from Bio import Entrez
+for argv in accession_number: # Insert for-loop to iterate argv for each accession_number
 
-# Entrez sends an email reqesting the data specified below
-Entrez.email = 'bheater@uab.edu'
-handle=Entrez.efetch(db=db,id=accession_number,rettype='gb') 
-# Accession id works, returns genbank format, looks in the 'nucleotide' database
+    # Grabbing genomes from Genbank
+    from Bio import Entrez
 
-# Store locally
-local_file=open(file_name,'w') # opens and create file (W)
-local_file.write(handle.read()) # write takes data and writes to file
-handle.close()
-local_file.close()
+    # Entrez sends an email reqesting the data specified below
+    Entrez.email = 'bheater@uab.edu'
+    handle=Entrez.efetch(db=db,id=accession_number,rettype='gb') 
+    # Accession id works, returns genbank format, looks in the 'nucleotide' database
 
-#Write Genbank File to BED file format: https://gist.github.com/brantfaircloth/893580
-from Bio import SeqIO
+    # Store locally
+    local_file=open(file_name,'w') # opens and create file (W)
+    local_file.write(handle.read()) # write takes data and writes to file
+    handle.close()
+    local_file.close()
 
-import pdb
+    #Write Genbank File to BED file format: https://gist.github.com/brantfaircloth/893580
+    from Bio import SeqIO
 
-def main():
-    outf = open( accession_number +'.bed', 'w')
-    for record in SeqIO.parse(open(file_name, "rU"), "genbank") :
-        for feature in record.features:
-            if feature.type == 'gene':
-                start = feature.location.start.position
-                stop = feature.location.end.position
-                try:
-                    name = feature.qualifiers['gene'][0]
-                except:
-                    # some features only have a locus tag
-                    name = feature.qualifiers['locus_tag'][0]
-                if feature.strand < 0:
-                    strand = "-"
-                else:
-                    strand = "+"
-                bed_line = accession_number + "\t{0}\t{1}\t{2}\t1000\t{3}\t{0}\t{1}\t65,105,225\n".format(start, stop, name, strand)
-                outf.write(bed_line)
-    outf.close()
+    import pdb
+
+    def main():
+        outf = open( accession_number +'.bed', 'w')
+        for record in SeqIO.parse(open(file_name, "rU"), "genbank") :
+            for feature in record.features:
+                if feature.type == 'gene':
+                    start = feature.location.start.position
+                    stop = feature.location.end.position
+                    try:
+                        name = feature.qualifiers['gene'][0]
+                    except:
+                        # some features only have a locus tag
+                        name = feature.qualifiers['locus_tag'][0]
+                    if feature.strand < 0:
+                        strand = "-"
+                    else:
+                        strand = "+"
+                    bed_line = accession_number + "\t{0}\t{1}\t{2}\t1000\t{3}\t{0}\t{1}\t65,105,225\n".format(start, stop, name, strand)
+                    outf.write(bed_line)
+        outf.close()
 
 
-if __name__ == '__main__':
-    main()
+    if __name__ == '__main__':
+        main()
 
-# Write Genbank File to GFF file format: http://biopython.org/wiki/GFF_Parsing
-from BCBio import GFF
-from Bio import SeqIO
- 
-in_file = file_name
-out_file = accession_number + '.gff'
-in_handle = open(in_file)
-out_handle = open(out_file, "w")
- 
-GFF.write(SeqIO.parse(in_handle, "genbank"), out_handle)
- 
-in_handle.close()
-out_handle.close()
+    # Write Genbank File to GFF file format: http://biopython.org/wiki/GFF_Parsing
+    from BCBio import GFF
+    from Bio import SeqIO
+     
+    in_file = file_name
+    out_file = accession_number + '.gff'
+    in_handle = open(in_file)
+    out_handle = open(out_file, "w")
+     
+    GFF.write(SeqIO.parse(in_handle, "genbank"), out_handle)
+     
+    in_handle.close()
+    out_handle.close()
 
 # Create new file, within which to place all BED files 
 file = open (accession_number +'.bed', 'a+')
