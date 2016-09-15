38004ffb440167d5349a111263fbb5cecdf5f843
bheater <bheater88@gmail.com>
2016-09-15 15:32:36 -0500

    Added comments and code for function to write hub.txt file and got the bed file saved in the newly created subdirectory.

--- accession_to_genome.py
+++ accession_to_genome.py
@@ -53,26 +53,36 @@ This script should
 		Purpose: provide information about research to build credibility of data 
 		and help people decide whether or not to use data
 '''
+# Create directory, named by accession #, to place relevant files
 
 # Use argv to obtain input of accession # from command line
 from sys import argv 
+from os import path
+import errno
+import os
+import shutil
 
 accession_number = argv[1] # Upacks argv-> assigned to 1 variable you can work with
 
+path_str = str(accession_number)
 
-# Create directory, named by accession #, to place relevant files
-from os import path
-import errno    
-import os
-path_str = './'+str(accession_number)
 def mkdir_p(path_str):
-    try:
-        os.makedirs(path_str)
-    except OSError as exc:  # Python >2.5
-        if exc.errno == errno.EEXIST and os.path.isdir(path_str):
-            pass
-        else:
-            raise
+	try:
+		os.makedirs(path_str)
+	except OSError as exc:  # Python >2.5
+		if exc.errno == errno.EEXIST and os.path.isdir(path_str):
+			pass
+		else:
+			raise
+	print "New directory, "+path_str+", created in the current directory."
+	subdir = path_str
+	filename = "NC_006273v2.bed"
+	dest_filepath = os.path.join(subdir, filename)
+	try:
+		shutil.copyfile(filename,dest_filepath)
+		print filename+" copied to subdirectory "+ subdir
+	except IOError:
+		print "Wrong path provided."
 
 def change_dir(path_str):
 	try:
@@ -87,15 +97,77 @@ def change_dir(path_str):
 #import genbank_to_bed12
 def accession_to_genome(accession_number):
 	from subprocess import call
-	cmd = ["python","genbank_to_bed12.py", "NC_006273.2"]
+	cmd = ["python","genbank_to_bed12.py", accession_number]
 	print 'calling: ' + ".".join(cmd)
 	call(cmd)
 	print 'accession_to_genome function running for ' + accession_number
 
+# 5. Automate conversion from bed file to bigbed file on cheaha
+# 	a. Create chrom.sizes file
+# 		1. If known database, $ fetchChromSizes <db> > <db>.chrom.sizes
+# 		2. If new database, determine the number of nucleotides in the chromosomes
+# 	b. Sort the bed file: $ sort -k1,1 -k2,2n unsorted.bed > sorted.bed
+# 	c. Convert bed to bigbed
+# 		1. $ module load ngs-ccts/ucsc_kent/2014-03-05
+# 		2. $ bedToBigBed in.bed hg19.chrom.sizes out.bb
+# 	d. Save bigbed file in directory
 
-if __name__ == '__main__':
-	mkdir_p(accession_number)
-	#change_dir(accession_number)
-	accession_to_genome(accession_number)
+# 6. Create the hub.txt file and save in directory on server
+# 	hub hub_name 
+# 	shortLabel hub_short_label
+# 	longLabel hub_long_label
+# 	genomesFile genomes_filelist
+# 	email email_address
+# 	descriptionUrl descriptionUrl
+def mk_hub_txt_file(accession_number):
+	hub_name = 'hub.txt'
+	hub_short_label= 'Viruses-HH5(HCMV)'
+	hub_long_label= 'Track hub for HCMV strain from RefSeq and GenBank'
+	genomes_filelist= 'genomes.txt'
+	email_address = 'hh5trackhub@vo.uabgrid.uab.edu'
+	descriptionUrl = 'description.html'
 	
+	line1 = ' '.join(['hub',hub_name])
+	line2 = ' '.join(['shortLabel', hub_short_label])
+	line3 = ' '.join(['long_label', hub_long_label])
+	line4 = ' '.join(['genomesFile', genomes_filelist])
+	line5 = ' '.join(['email', email_address])
+	line6 = ' '.join(['descriptionUrl', descriptionUrl])
+	hub_list_of_lines = [line1,line2,line3,line4,line5,line6]
+	filename = hub_name
+	hub_file_open = open(filename,'w')
+	hub_file_contents = '\n'.join([line1,line2,line3,line4,line5,line6])
+	print hub_file_contents
+	for line in hub_list_of_lines:
+		hub_file_open.writelines(line+'\n')
+	hub_file_open.close()
 
+
+#def mk_descriptionUrl_file():
+	'''
+	<html>
+	<body>
+	<h1>hello world</h1>
+	This is HCMV (HH5) strain Merlin and strain BE/7/2011, for starters.
+
+	Support contact: hh5trackhub@vo.uabgrid.uab.edu 
+	</body>
+	</html>
+	'''
+#def mk_genomes_file():
+	'''
+	genome hh5Merlin2
+	trackDb hh5Merlin2/trackDb.txt
+	twoBitPath hh5Merlin2/NC_006273v2.2bit
+	groups hh5Merlin2/groups.txt
+	description NC_006273v2
+	organism HH5 strain Merlin
+	defaultPos NC_006273v2:1-235646
+	orderKey 100
+	scientificName Human herpesvirus 5
+	htmlPath description.html
+	'''
+if __name__ == '__main__':
+	accession_to_genome(accession_number)
+	mkdir_p(accession_number)
+	mk_hub_txt_file(accession_number)
