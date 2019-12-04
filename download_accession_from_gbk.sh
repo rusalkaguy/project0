5f01c1813b72257aa710570c8cc774d6fa993b20
Curtis Hendrickson <curtish@uab.edu>
2019-12-04 11:20:02 -0600

    make Write_to_BED/GFF.py executable; fix bug and rename downloader.

--- download_accession_from_gbk.sh
+++ download_accession_from_gbk.sh
@@ -3,6 +3,6 @@
 # download genbank record
 #
 accession=NC_006273
-if [ ! -z "$1" ]; then acccession=$1; fi
+if [ ! -z "$1" ]; then echo "accession=$1"; accession=$1; fi
 
 wget -O ${accession}.gbk "https://www.ncbi.nlm.nih.gov/sviewer/viewer.cgi?tool=portal&save=file&log$=seqview&db=nuccore&report=genbank&id=${accession}&conwithfeat=on&withparts=on&hide-cdd=on"
